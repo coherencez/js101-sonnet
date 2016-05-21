6ccc9f09580c07589cd9a833a69f57ea78a08861
Thomas Swann <tkswann@mac.com>
2016-05-21 02:44:30 -0500

    hideious stylsheet added

--- sonnet.js
+++ sonnet.js
@@ -1,7 +1,7 @@
-String.prototype.replaceAll = function(search, replacement) {
-    var target = this;
-    return target.replace(new RegExp(search, 'g'), replacement);
-};
+// String.prototype.replaceAll = function(search, replacement) {
+//     var target = this;
+//     return target.replace(new RegExp(search, 'g'), replacement);
+// };
 
 
 
@@ -9,20 +9,18 @@ String.prototype.replaceAll = function(search, replacement) {
 // 		re = new RegExp(wordForRegex, "g");
 
 var sonDiv = document.getElementById("sonnet").innerHTML,
-	  outputList = document.getElementById("jsOutput");
-var sonLength = sonDiv.length;
-var indexOfOrphans = sonDiv.indexOf("orphans");
-var sonString = sonDiv.toString();
-var sonWinterReplace = sonString.replace("winter", "yueltide");
-var sonTheReplace = sonDiv.replaceAll("the", "a large");
-												// replace(/the/g, "a large");
-
+	  outputList = document.getElementById("jsOutput"),
+	  sonLength = sonDiv.length,
+	  indexOfOrphans = sonDiv.indexOf("orphans"),
+	  sonWinterReplace = sonDiv.replace("winter", "yueltide"),
+	  sonTheReplace = sonDiv.replace(/the\b/g, "a large");
+											//  .replaceAll("the", "a large");
 
 
 outputList.innerHTML += '<li>The length of the sonnet is <strong>' + sonLength + '</strong> characters long (including spaces).</li>';
 outputList.innerHTML += '<li>The word "orphans" begins at index <strong>[' + indexOfOrphans + ']</strong></li>';
-outputList.innerHTML += '<li>Here we replace the first occurence of "winter" with "yueltide". <p>' + sonWinterReplace + '</p></li>'
-outputList.innerHTML += '<li>Now we replace every occurence of "the" with "a large". <p>' + sonTheReplace + '</p></li>';
+outputList.innerHTML += '<li>Here we replace the first occurence of "winter" with "yueltide". <p>' + sonWinterReplace + '</p></li>';
+outputList.innerHTML += '<li>Now we replace every occurence of "the" with "a large". (now with more precise RegEx!) <p>' + sonTheReplace + '</p></li>';
 // outputList.innerHTML
 // outputList.innerHTML
 // outputList.innerHTML
