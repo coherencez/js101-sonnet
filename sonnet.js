408e9df690f0512771ddf4a62b32da34ad9ced37
Thomas Swann <tkswann@mac.com>
2016-05-22 21:25:14 -0500

    fixed var names to be more clear

--- sonnet.js
+++ sonnet.js
@@ -8,19 +8,19 @@
 // var wordForRegex = "the",
 // 		re = new RegExp(wordForRegex, "g");
 
-var sonDiv = document.getElementById("sonnet").innerHTML,
+var sonnetDiv = document.getElementById("sonnet").innerHTML,
 	  outputList = document.getElementById("jsOutput"),
-	  sonLength = sonDiv.length,
-	  indexOfOrphans = sonDiv.indexOf("orphans"),
-	  sonWinterReplace = sonDiv.replace("winter", "yueltide"),
-	  sonTheReplace = sonDiv.replace(/the\b/g, "a large");
+	  sonnetLength = sonnetDiv.length,
+	  indexOfOrphans = sonnetDiv.indexOf("orphans"),
+	  sonnetWinterReplace = sonnetDiv.replace("winter", "yueltide"),
+	  sonnetTheReplace = sonnetDiv.replace(/(the\b)/g, "a large");
 											//  .replaceAll("the", "a large");
 
 
-outputList.innerHTML += '<li>The length of the sonnet is <strong>' + sonLength + '</strong> characters long (including spaces).</li>';
+outputList.innerHTML += '<li>The length of the sonnet is <strong>' + sonnetLength + '</strong> characters long (including spaces).</li>';
 outputList.innerHTML += '<li>The word "orphans" begins at index <strong>[' + indexOfOrphans + ']</strong></li>';
-outputList.innerHTML += '<li>Here we replace the first occurence of "winter" with "yueltide". <p>' + sonWinterReplace + '</p></li>';
-outputList.innerHTML += '<li>Now we replace every occurence of "the" with "a large". (now with more precise RegEx!) <p>' + sonTheReplace + '</p></li>';
+outputList.innerHTML += '<li>Here we replace the first occurence of "winter" with "yueltide". <p>' + sonnetWinterReplace + '</p></li>';
+outputList.innerHTML += '<li>Now we replace every occurence of "the" with "a large". (now with more precise RegEx!) <p>' + sonnetTheReplace + '</p></li>';
 // outputList.innerHTML
 // outputList.innerHTML
 // outputList.innerHTML
